15d1ca16390b69d4c7dff260e422ebfc9adbc7ea
Deilon <deilast80@gmail.com>
2022-11-17 08:37:07 -0800

    Host to firebase

--- src/app/dummy-api.service.ts
+++ src/app/dummy-api.service.ts
@@ -8,8 +8,14 @@ export class DummyApiService {
 
   constructor(private http: HttpClient) { }
 
-  initArticles(category: string, page: number, pageSize: number, country: string) {
-    return this.http.get('http://localhost:3000/category?name=' + category);
-  }
+  // For local host / development plan
+  // initArticles(category: string, page: number, pageSize: number, country: string) {
+  //   return this.http.get('http://localhost:3000/category?name=' + category);
+  // }
 
+  // for online hosting when you're not paying for business/commercial newsapi.org plan
+  initArticles(category: string, page: number, pageSize: number, country: string) {
+    return this.http.get('https://newsnub.herokuapp.com/category?name=' + category);
+  }
+  
 }
